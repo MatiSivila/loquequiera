4db8aa331df769ecd1960f1631a386891c9371b5
unknown <MasiSivila>
2025-03-12 12:08:58 -0300

    Archivo de prueba

--- TrabajoPractico_1/proyecto_1/modules/modulo1.py
+++ TrabajoPractico_1/proyecto_1/modules/modulo1.py
@@ -1,2 +1,7 @@
 # módulo para organizar funciones o clases utilizadas en nuestro proyecto
 # Crear tantos módulos como sea necesario para organizar el código
+def funcion():
+    print("prueba")
+
+def funcion():
+    print("prueba distinta")
